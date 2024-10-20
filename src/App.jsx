fb21142dd07e001f0f531ab10400ca31388117c9
pras75299 <singh.prashantking@gmail.com>
2024-10-21 01:18:38 +0530

    basic web based wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
+import { generateMnemonic } from "bip39";
 import "./App.css";
+import { SolanaWallet } from "./SolanaWallet";
+import { EthWallet } from "./EthWallet";
 
 function App() {
+  const [mnemonic, setMnemonic] = useState("");
   return (
     <>
       <h1>Web Based Wallet</h1>
+      <input type="text" value={mnemonic}></input>
+      <button
+        onClick={async function () {
+          const mn = await generateMnemonic();
+          setMnemonic(mn);
+        }}
+      >
+        Create Seed Phrase
+      </button>
+      <SolanaWallet />
+      <EthWallet />
     </>
   );
 }
